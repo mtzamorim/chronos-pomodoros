ae034a22fd7c549fa704e5278f751287b40a1465
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-08-16 11:31:21 -0300

    refactor: ajustado app com componente container

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>
-            LOGO
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
 
-      <div className='container'>
-        <div className='content'>
-          <section>
-            MENU
-          </section>
-        </div>
-      </div>
-
-      <div className='container'>
-        <div className='content'>
-          <section>
-            FORM
-          </section>
-        </div>
-      </div>
-
-      <div className='container'>
-        <div className='content'>
-          <section>
-            FOOTER
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
