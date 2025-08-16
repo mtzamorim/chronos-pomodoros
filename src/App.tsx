63e52b1388c4c2bc4fe0e3f648fed5d3928739c9
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-08-16 11:15:38 -0300

    feat: adicionado layout inicial no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-        debitis voluptatibus quae iure ipsum harum ipsam. Minima animi libero,
-        aut, explicabo, cumque iste sit consequuntur ratione nam est ipsa cum.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>
+            MENU
+          </section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>
+            FORM
+          </section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>
+            FOOTER
+          </section>
+        </div>
+      </div>
     </>
   );
 }
