e196b2e9ab38e33fe30c8cd5bd53da454b75551e
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-08-17 09:37:44 -0300

    feat: adicionado botão padrão

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
 import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -43,10 +45,12 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>
+
+      <Container>asdf</Container>
     </>
   );
 }
