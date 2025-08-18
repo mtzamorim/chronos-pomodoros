d8d784ece0e21426d276216af840654db8ba79bd
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-08-18 20:07:00 -0300

    feat: layout página notfound

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function Home() {
+  return (
+    <MainTemplate>
+      <Container>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+        debitis in doloribus? Dolorem nesciunt saepe id ex et quis. Quibusdam
+        sapiente debitis quam mollitia animi facere, quisquam non perspiciatis
+        minima!
+      </Container>
+    </MainTemplate>
+  );
+}
