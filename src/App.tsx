e6a346d8bf5b663adb8138b534e3842cadd82631
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-08-18 20:05:43 -0300

    feat: ajustado template de página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,7 @@
-import { PlayCircleIcon } from 'lucide-react';
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { DefaultInput } from './components/DefaultInput';
-import { Footer } from './components/Footer';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-
+import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuInput'
-              type='text'
-              labelText='task'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
