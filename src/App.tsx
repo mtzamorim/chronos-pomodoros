66a802bf668b9790b3332d18b613528fde98823f
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-08-23 18:18:47 -0300

    feat: Main form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsReamining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
+  const [state, setState] = useState(initialState);
+
   return <Home />;
 }
