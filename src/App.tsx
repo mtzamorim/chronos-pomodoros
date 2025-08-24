4140d19dca4ba2c0112e2f998c8091f96e4762b9
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-08-24 10:12:39 -0300

    feat: criado contexto para distribuir informações para outros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { useState } from 'react';
-import type { TaskStateModel } from './models/TaskStateModel';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsReamining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 export function App() {
-  const [state, setState] = useState(initialState);
-
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />;
+    </TaskContextProvider>
+  );
 }
