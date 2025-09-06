77f9236507ea130ddf6073fb09198b4a5d61dbe1
Matheus Amorim <matheusfelix.amorim123@gmail.com>
2025-09-06 10:54:51 -0300

    feat: worker para contador e adicionar efeitos sonoros

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,8 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
+import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,6 +10,25 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+
+  const worker = TimerWorkerManager.getInstance()
+
+  worker.onmessage(e => {
+    const countDownSeconds = e.data
+
+    if(countDownSeconds <= 0) {
+      worker.terminate()
+    }
+  })
+
+  useEffect(() => {
+    if(!state.activeTask){
+      console.log('Worker terminado por falta de activeTask')
+      worker.terminate()
+    }
+
+    worker.postMessage(state);
+  }, [state, worker])
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
--- src/workers/timerWorker.js
+++ src/workers/timerWorker.js
@@ -1,3 +1,25 @@
-self.onmessage = function(event) {
-  console.log('WORKER recebeu: ', event.data)
-}
+let isRunning = false;
+
+self.onmessage = function (event) {
+  if (isRunning) return;
+
+  isRunning = true;
+
+  const state = event.data;
+  const {activeTask, secondsRemaining} = state;
+
+  const endDate = activeTask.startDate + secondsRemaining * 1000;
+  const now = Date.now();
+  let countDownSeconds = Math.ceil((endDate - now) / 1000)  
+
+  function tick(){
+    self.postMessage(countDownSeconds);
+
+    const now = Date.now();
+    countDownSeconds = Math.floor((endDate - now) / 1000)
+
+    setTimeout(tick, 1000);
+  }
+
+  tick();
+};
